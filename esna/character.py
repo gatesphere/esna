0c122dd68b1fce4d8b633f2842461b4c0b84235d
Jacob Peck <gatesphere@gmail.com>
2014-09-26 16:13:16 -0400

    Skeleton of a character class.

---
+++ esna/character.py
@@ -0,0 +1,21 @@
+#@+leo-ver=5-thin
+#@+node:peckj.20140926151523.4090: * @file character.py
+#@@language python
+
+#@+<< imports >>
+#@+node:peckj.20140926151523.4091: ** << imports >>
+#@-<< imports >>
+#@+<< declarations >>
+#@+node:peckj.20140926151523.4092: ** << declarations >>
+#@-<< declarations >>
+
+#@+others
+#@+node:peckj.20140926151523.4093: ** class Character
+class Character:
+  #@+others
+  #@+node:peckj.20140926151523.4094: *3* __init__
+  def __init__(self):
+    pass
+  #@-others
+#@-others
+#@-leo
